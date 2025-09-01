0200839120dbd33b06c6880a72125587e2dc6bd3
Santi <santichd@hotmail.com>
2025-09-01 00:14:54 -0500

    Login Frontend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -40,7 +41,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.material.v1110)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
